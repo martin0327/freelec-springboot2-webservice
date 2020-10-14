f908ac6d11a5f44d3cf37cd69eeaa988b5f9c333
Martin Yang <martin.yang@lge.com>
2020-10-14 15:47:48 +0900

    up to chapter 5.4

--- src/main/java/com/lge/book/springboot/web/IndexController.java
+++ src/main/java/com/lge/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.lge.book.springboot.web;
 
+import com.lge.book.springboot.config.auth.LoginUser;
 import com.lge.book.springboot.config.auth.dto.SessionUser;
 import com.lge.book.springboot.service.posts.PostsService;
 import com.lge.book.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
